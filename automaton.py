17389563b0ec4ae68a4eec3c24d94d98b704566e
Radu Mihai Gheorghe <radu2002ro@gmail.com>
2022-03-17 00:02:17 +0200

    Tema 1

--- automaton.py
+++ automaton.py
@@ -6,7 +6,6 @@ class Automaton():
 
     def validate(self):
         """Return a Boolean
-
         Returns true if the config file is valid,
         and raises a ValidationException if the config is invalid.
         """
@@ -14,21 +13,60 @@ class Automaton():
 
     def accepts_input(self, input_str):
         """Return a Boolean
-
         Returns True if the input is accepted,
         and it returns False if the input is rejected.
         """
         pass
 
     def read_input(self, input_str):
-        """Return the automaton's final configuration
-        
-        If the input is rejected, the method raises a
-        RejectionException.
-        """
-        pass
-    
+        input = open(input_str, "r")
+        alf = set()
+        states = set()
+        fstates = set()
+        transitions = []
+        cnt = 0
+        text_input = input.readlines()
+        for linie in text_input:
+            if linie.startswith('\n') or linie.startswith("#") or 'States' in linie or 'Transitions' in linie:
+                continue
+            elif "Sigma" in linie:
+                cnt = 1
+            elif linie.startswith('End'):
+                cnt += 1
+                continue
+            if cnt == 1:
+                alf.add(linie.strip())
+            elif cnt == 2:
+                if len(linie.strip().split(', ')) > 2:
+                    states.add(linie.strip().split()[0])
+                    if linie.strip().split(',')[2] == 'F' or linie.strip().split(',')[1] == 'F':
+                        fstates.add(linie.strip().split(' ,')[0])
+                    elif linie.strip().split(',')[2] == 'S' or linie.strip().split(',')[1] == 'S':
+                        istate = linie.strip().split(' ,')[0]
+                elif len(linie.strip().split(',')) == 2:
+                    states.add(linie.strip().split()[0])
+                    if linie.strip().split(',')[1] == 'S':
+                        istate = linie.strip().split(' ,')[0]
+                    elif linie.strip().split(',')[1] == 'F':
+                        fstates.add(linie.strip().split(' ,')[0])
+                else:
+                    states.add(linie.strip())
+            if cnt == 3:
+                text = linie.strip().split(", ")
+                for i in range(len(text)):
+                    if i == 1:
+                        text[i] = text[i][:-1]
+                transitions.append(tuple(text))
+        ok = 1
+        for transition in transitions:
+            if transition[1] not in alf or transition[0] not in states or transition[2] not in states:
+                print("Invalid input.")
+                ok = 0
+                return 0
+        if ok == 1:
+            print("Valid input. GG.")
+        return 1
 
 if __name__ == "__main__":
-    a = Automaton('your_config_file')
-    print(a.validate())
+    a = Automaton('date.in')
+    a.read_input('date.in')
